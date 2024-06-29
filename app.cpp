2f85d484c3395e2c224f0903fbcf49a7d76aa295
azurata <git@azurata.me>
2024-06-29 14:50:04 +0900

    init

---
+++ app.cpp
@@ -0,0 +1,14 @@
+#include "ev3api.h"
+#include "app.h"
+
+#define DEBUG
+
+#ifdef DEBUG
+#define _debug(x) (x)
+#else
+#define _debug(x)
+#endif
+
+void main_task(intptr_t unused) {
+  // Your code!
+}
